f30debd957b1acba15aa09107865bae9b803a12b
timothy omobolaji <newtontimos01@gmail.com>
2024-01-17 22:29:01 +0100

    niger country

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <img src="/images/nigeria.jpg" alt="" />
+      <h1 className="flag">Nigeria My Country</h1>
+      <p className="niger">
+        Nigeria, country located on the western coast of Africa. Nigeria has a
+        diverse geography, with climates ranging from arid to humid equatorial.
+        However, Nigeria’s most diverse feature is its people. Hundreds of
+        languages are spoken in the country, including Yoruba, Igbo, Fula,
+        Hausa, Edo, Ibibio, Tiv, and English. The country has abundant natural
+        resources, notably large deposits of petroleum and natural gas. Nigeria
+        Nigeria The national capital is Abuja, in the Federal Capital Territory,
+        which was created by decree in 1976. Lagos, the former capital, retains
+        its standing as the country’s leading commercial and industrial city.
+        Modern Nigeria dates from 1914, when the British Protectorates of
+        Northern and Southern Nigeria were joined. The country became
+        independent on October 1, 1960, and in 1963 adopted a republican
+        constitution but elected to stay a member of the Commonwealth.
+      </p>
+    </div>
+  );
+}
+
+export default App;
